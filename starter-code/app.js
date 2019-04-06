492a333ab09e0fff1c53ab80d0f840f64344e85a
Marcelo Oliveira <marbmo@gmail.com>
2019-04-06 17:10:42 -0300

    Exercício feito

---
+++ starter-code/app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const key = process.env.key;
+
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+const hbs = require('hbs');
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.get('/articles', (request, response, next) => {
+  fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+request.query.q+'&api-key='+key)
+    .then(data => data.json())
+    .then(data => {
+      response.render('articles', data.response);
+    });
+});
+
+
+app.listen(3000, () => console.log("Listening"));
